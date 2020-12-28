dc49c8c35053413f4610a29786a6e05943798691
Howard <934303306@qq.com>
2020-12-28 22:29:18 +0800

    2020/12/28 晚上 threading学习

---
+++ python_learning/threadind学习.py
@@ -0,0 +1,30 @@
+import threading
+import time
+"""
+学习使用threading模块
+
+"""
+
+class MyThread(threading.Thread):
+    def __init__(self, thread_name):
+        # 注意：一定要显式的调用父类的初始化函数。
+        super(MyThread, self).__init__(name=thread_name)
+
+    def run(self):
+        print("%s正在运行中......" % self.name)
+        print("运行时间为{}".format(time.ctime()))
+        time.sleep(2)
+        print("然后睡了10s,现在时间为{}".format(time.ctime()))
+
+        """Method representing the thread's activity.
+
+        You may override this method in a subclass. The standard run() method
+        invokes the callable object passed to the object's constructor as the
+        target argument, if any, with sequential and keyword arguments taken
+        from the args and kwargs arguments, respectively.
+
+        """
+
+if __name__ == '__main__':
+    for i in range(10):
+        MyThread("第{}个线程".format(i)).start()
