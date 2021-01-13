0b941a053a71866b4003ab3bebff2091c9be295e
Howard <934303306@qq.com>
2021-01-14 00:47:26 +0800

    request库学习，cookies没懂

--- python_learning/requests库学习.py
+++ python_learning/requests库学习.py
@@ -1,9 +1,13 @@
 from http import cookiejar
 import requests
-my_logindata = "username=%E9%BB%84%E6%B5%A9&password=huanghao123&logout=7days&returnto=torrents.php"
-r = requests.post(url=r"https://www.tjupt.org/takelogin.php",data=my_logindata)
-mycookies = r.cookies
-headers = {'user-agent': r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-'cookies':mycookies}
-r = requests.get(url=r"https://www.tjupt.org/index.php",headers=headers)
-print(r.text)
+headers = {'user-agent': r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
+
+# 访问登陆页面
+r0 = requests.get(url=r"https://www.tjupt.org/login.php",headers=headers)
+print("登陆页面访问",r0.status_code)
+
+# 登录账号
+r1 = requests.post(url=r"https://www.tjupt.org/takelogin.php",data="username=%E9%BB%84%E6%B5%A9&password=huanghao123&logout=7days")
+a = r1.cookies.get_dict()
+print(r1.status_code)
+print(r1.text)
