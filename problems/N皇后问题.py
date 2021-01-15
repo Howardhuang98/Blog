b9e940e16003a4acef29d004b8d9da6637082647
Howard <934303306@qq.com>
2021-01-15 23:00:28 +0800

    递归回溯问题

---
+++ problems/N皇后问题.py
@@ -0,0 +1,60 @@
+"""
+n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+
+每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-queens
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> list:
+        def generateBoard():
+            board = list()
+            for i in range(n):
+                row[queens[i]] = "Q"
+                board.append("".join(row))
+                row[queens[i]] = "."
+            return board
+
+        def backtrack(row: int):
+            if row == n:
+                board = generateBoard()
+                solutions.append(board)
+            else:
+                for i in range(n):
+                    if i in columns or row - i in diagonal1 or row + i in diagonal2:
+                        continue # 如果i不能下就跳过此次循环
+                    queens[row] = i
+                    columns.add(i) # 第i列被占
+                    diagonal1.add(row - i) #对角
+                    diagonal2.add(row + i)#对角
+                    backtrack(row + 1) # 考虑下一行
+                    columns.remove(i)
+                    diagonal1.remove(row - i)
+                    diagonal2.remove(row + i)
+
+        solutions = list()
+        queens = [-1] * n
+        columns = set()
+        diagonal1 = set()
+        diagonal2 = set()
+        row = ["."] * n
+        backtrack(0)
+        return solutions
+
+if __name__ == '__main__':
+    solu = Solution()
+    a = solu.solveNQueens(4)
+    print(a)
+
+
+
+
+
+
