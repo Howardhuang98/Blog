15d165558ae654b102f8ff7333b5de94bbba3c61
Howard <934303306@qq.com>
2021-03-04 13:27:40 +0800

    插入排序 升降序

--- problems/插入排序.py
+++ problems/插入排序.py
@@ -17,3 +17,20 @@ def insertion_sort(A: list):
 
 result = insertion_sort([1, 3, 2, 5, 7, 9])
 print(result)
+
+"""
+重写插入排序使结果为升序
+"""
+def insertion_sort_reversed(a:list):
+    for j in range(1,len(a)):
+        key = a[j]
+        i = j - 1
+        while i>=0 and a[i]< key:
+            a[i+1] = a[i]
+            i -= 1
+        a[i+1] = key
+
+    return a
+
+result =  insertion_sort_reversed([31,41,59,26,41,58])
+print(result)
