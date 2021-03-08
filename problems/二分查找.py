1a8ec13743475b1e04207635f5cdea230e7f2173
Howard <934303306@qq.com>
2021-03-08 15:25:19 +0800

    reformat

--- problems/二分查找.py
+++ problems/二分查找.py
@@ -1,33 +1,33 @@
-def binary_search(a:list,target:int):
+def binary_search(a: list, target: int):
     a = sorted(a)
     Left = 0
     right = len(a)
-    index = int((Left+right)/2)
-    while a[index]!=target and abs(right-Left)>1:
-        if a[index]>target:
+    index = int((Left + right) / 2)
+    while a[index] != target and abs(right - Left) > 1:
+        if a[index] > target:
             right = index
-            index = int((index+Left)/2)
-            
+            index = int((index + Left) / 2)
+
         else:
             left = index
-            index = int((index+right)/2)
+            index = int((index + right) / 2)
 
-    if a[index]!=target:
+    if a[index] != target:
         print("target was not found")
-    else:        
+    else:
         return index
 
-    
-def binary_search2(a,target):
-    mid = int(len(a)/2)
-    if a[mid]==target:
+
+def binary_search2(a, target):
+    mid = int(len(a) / 2)
+    if a[mid] == target:
         return mid
-    if a[mid]<target:
-        return binary_search2(a[mid:],target)
-    if a[mid]> target:
-        return binary_search2(a[:mid],target)
+    if a[mid] < target:
+        return binary_search2(a[mid:], target)
+    if a[mid] > target:
+        return binary_search2(a[:mid], target)
 
 
 if __name__ == "__main__":
-    index = binary_search2(a=[1,2,4,234,588,600],target = 600)
+    index = binary_search2(a=[1, 2, 4, 234, 588, 600], target=600)
     print(index)
