9f96d640647c10582c258e78433b42a21a0588c6
Howard <934303306@qq.com>
2021-03-10 19:36:58 +0800

    矩阵乘法 遍历

---
+++ problems/矩阵乘法.py
@@ -0,0 +1,32 @@
+"""
+输入变量为
+a = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+的矩阵计算
+a*b = c
+"""
+
+"""
+遍历法，复杂度为n^3
+"""
+
+
+def matrix_multiply(a, b):
+    n = len(a)
+    row = []
+    c = []
+    for i in range(0, n):
+        for j in range(0, n):
+            num = 0
+            for k in range(0, n):
+                num += a[i][k] * b[k][j]
+            row.append(num)
+        c.append(row)
+        row = []
+    return c
+
+
+if __name__ == '__main__':
+    a = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+    b = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+    c = matrix_multiply(a, b)
+    print(c)
