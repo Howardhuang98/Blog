bf21e0701e79255327b55b0e92175f37a08aeddb
Howard <934303306@qq.com>
2021-03-17 21:20:53 +0800

    堆排序 未完成

--- problems/堆排序.py
+++ problems/堆排序.py
@@ -23,20 +23,45 @@ class Heap:
         """
         l = self.left_index(i)
         r = self.right_index(i)
-        if l <= len(self.list) and self.list[l] > self.list[i]:
-            largest = l
-        else:
-            largest = i
-        if r <= len(self.list) and self.list[r] > self.list[largest]:
-            largest = r
-        if largest != i:
-            self.list[i], self.list[largest] = self.list[largest], self.list[i]
-            self.max_heapify(largest)
+        if r < len(self.list):  # 左右都有子节点
+            if self.list[l] > self.list[r] and self.list[l] > self.list[i]:
+                self.list[l], self.list[i] = self.list[i], self.list[l]
+                self.max_heapify(l)
+            if self.list[r] > self.list[l] and self.list[r] > self.list[i]:
+                self.list[r], self.list[i] = self.list[i], self.list[r]
+                self.max_heapify(r)
+        if l < len(self.list) and r >= len(self.list):  # 只有左
+            if self.list[l] > self.list[i]:
+                self.list[l], self.list[i] = self.list[i], self.list[l]
+        if l >= len(self.list):  # 无子节点
+            pass
+
+        return self.list
+
         return self.list
 
 
-if __name__ == '__main__':
-    a = [16, 4, 10, 8, 7, 9, 3, 2, 4, 1]
+def build_max_heap(a):
     heapa = Heap(a)
-    a = heapa.max_heapify(1)
+    for i in range((len(a) - 1) // 2, -1, -1):
+        heapa.max_heapify(i)
+    return heapa.list
+
+
+def heap_sort(a):
+    r = 0
+    heapa = Heap(a)
+    a = build_max_heap(a)
+    for i in range((len(a) - 1), 0, -1):
+        heapa.list[0], heapa.list[i] = heapa.list[i], heapa.list[0]
+        heapa.max_heapify(0)
+    return a
+
+
+if __name__ == '__main__':
+    a = [16, 20, 10, 8, 7, 9, 100]
+    a = build_max_heap(a)
     print(a)
+    b = [16, 20, 10, 8, 7, 9, 100]
+    b = heap_sort(b)
+    print(b)
