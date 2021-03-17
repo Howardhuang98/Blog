b44e224bad0eb84afb250473ff093785557c8db6
Howard <934303306@qq.com>
2021-03-17 22:29:15 +0800

    堆排序heapq调用

---
+++ problems/堆序列heapq调用.py
@@ -0,0 +1,21 @@
+import heapq
+
+"""
+python自带最大/最小堆算法，带有一系列的堆维护功能，可以直接调用
+实现最大优先队列，三个主要功能
+插入
+最大
+去掉最大
+增加
+
+"""
+
+
+def insert(a):
+    pass
+
+
+if __name__ == '__main__':
+    a = [5, 4, 8, 9]
+    heapq._heapify_max(a)  # 最大堆化
+    print(dir(heapq))
