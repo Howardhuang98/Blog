2a3d17cc254bb75258f00b172ae408c436a11879
Howard <934303306@qq.com>
2021-03-22 12:49:34 +0800

    reformat

--- problems/计数排序.py
+++ problems/计数排序.py
@@ -1,20 +1,21 @@
-def counting_sort(a:list):
+def counting_sort(a: list):
     k = max(a)
     b = []
     c = []
-    for i in range(k+1):
+    for i in range(k + 1):
         c.append(0)
     for j in range(len(a)):
         c[a[j]] += 1
         b.append(0)
-    for i in range(1,k+1):
-        c[i] = c[i]+c[i-1]
-    for j in range(len(a)-1,-1,-1):
-        b[c[a[j]]-1] = a[j]
-        c[a[j]] -=1
+    for i in range(1, k + 1):
+        c[i] = c[i] + c[i - 1]
+    for j in range(len(a) - 1, -1, -1):
+        b[c[a[j]] - 1] = a[j]
+        c[a[j]] -= 1
     return b
 
+
 if __name__ == '__main__':
-    a = [1,8,7,2,4,6]
+    a = [1, 8, 7, 2, 4, 6]
     b = counting_sort(a)
     print(b)
