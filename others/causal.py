e60ebafa5694049b56e6702b501b346644266b38
HUANG HAO <934303306@qq.com>
2021-11-17 23:06:54 +0800

    argsort理解.ipynb

---
+++ others/causal.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   causal.py    
+@Contact :   huanghoward@foxmail.com
+@Modify Time :    2021/11/15 15:02  
+------------      
+"""
+from causallearn.search.ScoreBased.GES import ges
+ges()
